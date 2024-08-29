3437ef027544dafedc8014c4f58688f2240a5f83
Breno Silva <brenosill@hotmail.com>
2024-08-29 00:10:52 -0300

    Update App.js
    Importando o toastify

--- react-app/src/paginas/App.js
+++ react-app/src/paginas/App.js
@@ -1,12 +1,21 @@
 import { Rotas } from '../routes';
 import './style.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function App() {  
 
 
   return (
     <>  
       <div className='container'>
-         <Rotas />
+
+        <ToastContainer 
+          position="top-right"
+          autoClose={3000} />
+
+            <Rotas />
+        
       </div>
 
     </>
