313e849b2a5d4781a5b125a796c782011b98a1c4
Breno Silva <brenosill@hotmail.com>
2024-08-29 00:11:10 -0300

    Update routes.js
    Adiciona rota de favoritos

--- react-app/src/routes.js
+++ react-app/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { Home } from "./paginas/home/home";
 import Filme from "./paginas/Filme";
 import Error404 from "./paginas/Erro";
+import Favoritos from "./paginas/favoritos";
 
 
 export function Rotas(){
@@ -11,7 +12,7 @@ export function Rotas(){
             <Routes>
                 <Route path="/" element={<Home />}/>     
                 <Route path="/filme/:id" element={<Filme />} />  
-           
+                <Route path="/favoritos" element={<Favoritos />}/>    
                 <Route path="*" element={<Error404 />}/>     
             </Routes>    
     
